190dd5e250fa90735368c44225cd3105775671f0
Matheus <matheuslevit@gmail.com>
2025-11-18 22:00:05 -0300

    fix: don't add a new line in Input.read

--- src/tictactoe/io/Input.java
+++ src/tictactoe/io/Input.java
@@ -6,7 +6,7 @@ public class Input {
     private Input() {}
 
     public static String read(String message) {
-        if (message != null) Output.write(message + " ");
+        if (message != null) Output.write(message + " ", false);
 
         return new Scanner(System.in).nextLine();
     }
