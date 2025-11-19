48e312193d3ede3526bfb19b35d1e696a302a8d3
Matheus <matheuslevit@gmail.com>
2025-11-18 21:59:32 -0300

    fix: catch any typing error and repeat round

--- src/tictactoe/core/Game.java
+++ src/tictactoe/core/Game.java
@@ -1,10 +1,31 @@
 package tictactoe.core;
 
+import tictactoe.io.Input;
+import tictactoe.io.Output;
+
 public class Game {
     private final Board board = new Board();
     private final Players players = new Players();
 
     public void start() {
-        System.out.println(board);
+        while(true) {
+            Output.writeNewLine();
+            Output.write(board);
+
+            play(players.next());
+        }
+    }
+
+    private void play(Symbol symbol) {
+        while(true) {
+            try {
+                String play = Input.read(String.format("'%s' Play (Row, Col) =>", symbol));
+                Coordinate coordinate = Coordinate.parse(play);
+                board.update(symbol, coordinate);
+                break;
+            } catch(RuntimeException e) {
+                Output.write("ERROR: " + e.getMessage());
+            }
+        }
     }
 }
